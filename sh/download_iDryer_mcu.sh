092e38e58eb8d2e6289e896b7f72c82633bec9b4
Ru <pavluchenko.r@gmail.com>
2025-07-12 11:22:02 +0300

    refactoring

--- sh/download_iDryer_mcu.sh
+++ sh/download_iDryer_mcu.sh
@@ -4,30 +4,30 @@ REPO_URL="https://github.com/pavluchenkor/iDryer-Unit.git"
 TARGET_DIR="iDryer_mcu"
 TEMP_DIR=".temp_iDryer_clone"
 
-# Проверка наличия git
-command -v git >/dev/null 2>&1 || { echo >&2 "❌ Требуется git, но он не установлен."; exit 1; }
+# Checking git availability
+command -v git >/dev/null 2>&1 || { echo >&2 "❌ Requires git, but it is not installed."; exit 1; }
 
-# Удаляем старые данные
+# Delete old data
 rm -rf "$TEMP_DIR" "$TARGET_DIR"
 
-# Клонируем репозиторий без содержимого
+# Clone a repository without content
 git clone --depth 1 --filter=blob:none --no-checkout "$REPO_URL" "$TEMP_DIR" || {
-  echo "❌ Ошибка при клонировании репозитория"; exit 1;
+  echo "❌ Error when cloning a repository"; exit 1;
 }
 
 cd "$TEMP_DIR" || exit 1
 
-# Настройка sparse-checkout
+# Configuring sparse-checkout
 git sparse-checkout init --cone
 git sparse-checkout set "$TARGET_DIR"
 
 # Checkout основной ветки
-git checkout main || { echo "❌ Ошибка checkout"; exit 1; }
+git checkout main || { echo "❌ Error checkout"; exit 1; }
 
 cd ..
-mv "$TEMP_DIR/$TARGET_DIR" ./ || { echo "❌ Не удалось переместить папку"; exit 1; }
+mv "$TEMP_DIR/$TARGET_DIR" ./ || { echo "❌ Failed to move folder"; exit 1; }
 
-# Очистка
+# Erasing
 rm -rf "$TEMP_DIR"
 
-echo "✅ Папка $TARGET_DIR успешно скачана в текущую директорию"
+echo "✅ The $TARGET_DIR folder has been successfully downloaded to the current directory"
